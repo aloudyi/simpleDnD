5eba67d87cb0245a86234a19ce436313fa631879
zenon <35837688+aloudyi@users.noreply.github.com>
2022-07-24 13:54:13 +0200

    Delete classes directory

--- classes/Monster.py
+++
@@ -1,8 +0,0 @@
-from classes.Entity import Entity
-
-class Monster(Entity):
-    def __init__(self,name,race,max_hp=10,current_hp=10,state_duration=0,
-                 spells={},state="normal", modifier=0,description="",picture_url=""):
-        super().__init__(self,name,race,max_hp,current_hp,state_duration,
-                            spells,state, modifier,description,picture_url)
-        self.monster_class = name
